0d56b2d958fc3c6684f6eea5882f77da31b027d9
Andrea-Mattia <andrea.mattia94@gmail.com>
2021-04-14 16:30:00 +0200

    add htm and base css

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 $(document).ready(function () {
     
-    console.log('Works');
-    console.log('jQuery', $);
+    /**
+     * Descrizione
+     * Rifare la todo list come vista insieme a lezione:
+     * 1. popolando gli elementi della lista con JavaScript
+     * 2. inserendo un nuovo todo con un input testuale e gli eventi da tastiera
+     * 3. Rimozione todo con click su icona
+     * 4. Cliccando sul testo compare uno sbarramento a indicarne il completamento
+     */
+
+    
 
 // End doc ready
 });
