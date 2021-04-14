bf804161adb7a588b8de63aeb99eccae8c24c684
Andrea-Mattia <andrea.mattia94@gmail.com>
2021-04-14 17:08:01 +0200

    add items in todo list

--- js/script.js
+++ js/script.js
@@ -9,7 +9,54 @@ $(document).ready(function () {
      * 4. Cliccando sul testo compare uno sbarramento a indicarne il completamento
      */
 
-    
+    // Sorgente di dati (al momento inseriti a mano)
+    var todoItems = [
+        {
+            text: 'Seguire la lezione Boolean',
+            completed: true,
+        },
+        {
+            text: 'Fare l\'esercitazione pomeridiana',
+            completed: false,
+        },
+        {
+            text: 'Guardare di nuovo il meglio di AGG',
+            completed: false,
+        },
+        {
+            text: 'Provare a diventare di nuovo Batman',
+            completed: false,
+        },
+        {
+            text: 'Pulire il PC dalla polvere',
+            completed: true,
+        },
+    ];
+
+    // References
+    var list = $('.todos');
+    var newInput = $('.add-todo');
+    var template = $('.template li');
+
+    // 1. Inserimento di elementi nella todo list
+    for (var i = 0; i < todoItems.length; i++) {
+        // assegno ad una variabile dinamicamente i valori dell'array todoItems
+        var todo = todoItems[i];
+        // faccio un clone del template e lo assegno ad una variabile
+        var item = template.clone();
+        // cerco lo span 'text' e ci inserisco il valore text dell'oggetto
+        item.find('.text').text(todo.text);
+
+        // se per caso l'item ha un true come valore di completed gli aggiungo la classe completed per farci una linea sopra
+        if (todo.completed) {
+            item.find('.text').addClass('completed');
+        }
+
+        // inserisco nell'html i miei item
+        list.append(item);
+    }
+
+
 
 // End doc ready
 });
